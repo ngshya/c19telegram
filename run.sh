520d22d38001e0ec319b386ed8d68fa46895a8e5
ngshya <ngshya@gmail.com>
2020-10-27 12:04:31 +0100

    telegram-send --file

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ python generate_plots.py
 echo "[telegram]" >> telegram-send.conf
 echo "token = $token" >> telegram-send.conf
 echo "chat_id = $chat_id" >> telegram-send.conf
-telegram-send --config telegram-send.conf -i COVID-19-ITALIA.pdf
+telegram-send --config telegram-send.conf --file COVID-19-ITALIA.pdf
